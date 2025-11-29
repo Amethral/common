41cee7cc73087c1dcbaa7aa051f4831581b033f7
Constant <constantsuchet@gmail.com>
2025-11-29 10:02:07 +0100

    removed dependencies

--- Amethral.common/Requests.cs
+++ Amethral.common/Requests.cs
@@ -1,29 +1,18 @@
-using System.ComponentModel.DataAnnotations;
-
 namespace Amethral.Common.DTOs
 {
     public class RegisterRequest
     {
-        [Required]
-        [MinLength(3, ErrorMessage = "Username must be at least 3 characters")]
         public string Username { get; set; } = string.Empty;
-
-        [Required]
-        [EmailAddress]
+        
         public string Email { get; set; } = string.Empty;
-
-        [Required]
-        [MinLength(6, ErrorMessage = "Password must be at least 6 characters")]
+        
         public string Password { get; set; } = string.Empty;
     }
 
     public class LoginRequest
     {
-        [Required]
-        [EmailAddress]
         public string Email { get; set; } = string.Empty;
-
-        [Required]
+        
         public string Password { get; set; } = string.Empty;
     }
     public class OAuthLinkRequest
